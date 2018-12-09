be8601bdbc2693ca0e8b9c677c536496a0ea8527
João Dematé <joaodematejr@gmail.com>
2018-12-09 21:27:16 -0200

    HTTP Request com Axios e o ciclo de vida de um componente App 4 finalizado

--- ProjetosRN/App4 Atualizado/scr/components/Itens.js
+++ ProjetosRN/App4 Atualizado/scr/components/Itens.js
@@ -27,7 +27,7 @@ const styles = StyleSheet.create({
         padding: 10,
         borderRadius: (Platform.OS === 'ios') ? 5 : 5,
         flexDirection: 'row',
-        backgroundColor: '#fff'
+        backgroundColor: '#fff',
     },
     foto: {
         width: 102,
