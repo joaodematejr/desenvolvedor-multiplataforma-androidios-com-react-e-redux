5b682796e32f4afed8b8e9c4b033a5e7b708b41f
João Dematé <joaodematejr@gmail.com>
2018-12-09 21:23:28 -0200

    Props, State e modularização de código Finalização app4

--- ProjetosRN/App4/App.js
+++ ProjetosRN/App4/App.js
@@ -4,17 +4,8 @@ import ListaItens from './scr/components/listaItens';
 export default class App extends Component {
   constructor(props) {
     super(props);
-    console.log("Construindo APP");
-  }
-  componentWillMount() {
-    console.log("Fazendo alguma coisa antes de renderizar");
-  }
-  componentDidMount() {
-    console.log("Fazendo alguma coisa depois de renderizar");
   }
   render() {
-    const teste = "esse é um valor ";
-    console.log(teste);
     return (
       <ListaItens />
     );
--- ProjetosRN/App4/scr/components/listaItens.js
+++ ProjetosRN/App4/scr/components/listaItens.js
@@ -1,10 +1,31 @@
+import axios from 'axios';
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { ScrollView } from 'react-native';
+import Itens from './Itens';
 
 export default class ListaItens extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { ListaItens: [] }
+    }
+
+    componentWillMount() {
+        //API 
+        //http://faus.com.br/recursos/c/dmairr/api/itens.html
+        axios.get('http://faus.com.br/recursos/c/dmairr/api/itens.html')
+            .then(response => {
+                this.setState({ ListaItens: response.data })
+                //console.log(response)
+            })
+            .catch(() => { console.log("Erro ao Recuperar os Dados") })
+    }
+
     render() {
         return (
-            <Text> teste importacao de componente </Text>
+            <ScrollView style={{ backgroundColor: '#DDD' }}>
+                {this.state.ListaItens.map(item => (<Itens key={item.titulo} item={item} />))}
+            </ScrollView>
         );
     }
 }
